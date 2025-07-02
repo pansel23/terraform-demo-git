daba42028e9f6c0aed874e969f97e70402e633a1
pansel23 <selvambrother@gmail.com>
2025-07-02 13:00:12 +0530

    Update backend.tf
    bucket name

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket23"
+    bucket         = "my-terraform-state-bucket23100495"
     key            = "ec2/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
